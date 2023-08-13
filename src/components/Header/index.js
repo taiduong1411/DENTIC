d65bf8e6c2f7ac095172f26a5e70f3189a47e4aa
DuongTrongTai <duongtai141120@gmail.com>
2023-08-14 01:28:33 +0700

    fix header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,7 +28,7 @@ function Header() {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Blog</span>
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white max-[1200px]:hidden">Blog</span>
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Logout />
